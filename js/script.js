e2d31b1f3f61a630244b347c9feda98d33db1be2
puncoviy <usaspinner@gmail.com>
2022-06-20 22:03:47 +0300

    feat: working at slider

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 $(function(){
 
 
-    $('.reviews-slider').slick({});
+    $('.reviews-slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false,
+        dots: true,
+    });
 
 
 })
