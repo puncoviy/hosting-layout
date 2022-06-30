c8a2582155f9df2e53e56669d556753866cbcec4
puncoviy <usaspinner@gmail.com>
2022-06-30 14:22:58 +0300

    feat: add responsive

--- js/script.js
+++ js/script.js
@@ -9,7 +9,35 @@ $(function(){
         dots: true,
         autoplay: true,
         autoplaySpeed: 3000,
+        responsive: [
+            {
+              breakpoint: 1240,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              }
+            }]
     });
 
 
+})
+
+
+const menuBtn = document.getElementById('burgerMenu');
+const menu = document.getElementById('menu');
+const div = document.querySelector('.burger__center');
+const shader = document.querySelector('.shader')
+
+menuBtn.addEventListener('click', function(){
+    menuBtn.classList.toggle('active');
+    div.classList.toggle('active');
+    menu.classList.toggle('active');
+    shader.classList.toggle('active');
+})
+
+shader.addEventListener('click', function(){
+    menuBtn.classList.toggle('active');
+    div.classList.toggle('active');
+    menu.classList.toggle('active');
+    shader.classList.toggle('active');
 })
